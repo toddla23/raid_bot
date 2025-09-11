693a5831eea5c54d48abe581921303613bc2dd3f
toddla23 <qkrwhdgur0319@gmail.com>
2025-09-11 17:34:55 +0900

    [Add] 파티참여 추가

--- src/commands/partyJoin.js
+++ src/commands/partyJoin.js
@@ -1,21 +1,86 @@
-const { SlashCommandBuilder } = require("discord.js");
-const role = require("../util/role");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const memberService = require("../service/raid/member.js");
+const partyService = require("../service/raid/party.js");
+const content = require("../util/content.js");
+
+const roleChoices = [
+  { name: "딜러", value: 0},
+  { name: "서포터", value: 1 },
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("파티참여")
     .setDescription("파티에 참여합니다.")
-    .addNumberOption(option =>
-      option.setName("party-id")
-        .setDescription("파티 번호")
-        .setRequired(true))
-    .addNumberOption(option =>
-      option.setName("role")
-        .setDescription("딜러 or 서폿")
+    .addStringOption(
+      (option) =>
+        option
+          .setName("파티명")
+          .setDescription("참여할 파티를 선택하세요")
+          .setRequired(true)
+          .setAutocomplete(true) // 자동완성 활성화
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("포지션")
+        .setDescription("딜러 또는 서포터")
         .setRequired(true)
-        .addChoices(...role)),
+        .addChoices(...roleChoices)
+    ),
 
+  // Interaction 처리
   async execute(interaction) {
-    await interaction.reply("파티에 참가했습니다!");
+    const partyId = interaction.options.getString("파티명");
+    const role = interaction.options.getNumber("포지션");
+    const userId = interaction.user.id;
+    const userName = interaction.user.globalName;
+    // console.log(partyId, role, userId, userName);
+
+    const result = await memberService.addMember(
+      partyId,
+      userName,
+      userId,
+      role
+    );
+
+    // console.log(await memberService.findByParty(partyId));
+    const result1 = await memberService.findUserIdByParty(partyId);
+    const embed = new EmbedBuilder()
+      .setTitle(`${result1.party.id}. ${result1.party.party_name}`)
+      .addFields(
+        {
+          name: "목표",
+          value: `${result1.party.name}`,
+        },
+        {
+          name: "딜러",
+          value: `닉네임 : ${result1.dealer.map((name) => {
+            return `<@${name.user_id}>, `;
+          })}`,
+        },
+        {
+          name: "서포터",
+          value: `닉네임 : ${result1.supporter.map((name) => {
+            return `<@${name.user_id}>, `;
+          })}`,
+        }
+      );
+    interaction.channel.send({ embeds: [embed] });
+  },
+
+  // 자동완성 처리
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    // console.log("hi")
+    const allParties = await partyService.findByName(focusedValue);
+    // console.log(allParties);
+    // findName에 검색어 전달해서 필터링 가능
+    const choices = allParties.map((p) => ({
+      name: `${p.party_name} (${p.contents})`,
+      value: `${p.id}`,
+    }));
+    await interaction.respond(
+      choices.slice(0, 25) // Discord는 최대 25개만
+    );
   },
 };
--- src/service/raid/party.js
+++ src/service/raid/party.js
@@ -1,74 +1,206 @@
 const connection = require("../../config/connection");
 
-
 const addParty = async (contentId, name, startTime) => {
   try {
-    const [result, field] = await connection.query("INSERT INTO party (`contents_id`, `party_name`, `start_time`) VALUES  (?)", [[contentId, name, startTime]]);
+    const [result, field] = await connection.query(
+      "INSERT INTO party (`contents_id`, `party_name`, `start_time`) VALUES  (?)",
+      [[contentId, name, startTime]]
+    );
     return result;
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 const findAllParty = async () => {
   try {
     let tempResult;
     const now = new Date();
-    const [results, field] = await connection.query("SELECT id, contents, party_name, start_time, cnt FROM party_member_count where start_time BETWEEN ? and ? ORDER BY start_time", [dateFormat(now), dateFormat(new Date(now.setDate(now.getDate() + 7)))]);
-    const result = await Promise.all(results.map(async (result) => {
-      const [dealers, dealerFields] = await connection.query("SELECT * from member WHERE party_id = ? and role = 0", [result.id]);
-      const [supporters, supporterFields] = await connection.query("SELECT * from member WHERE party_id = ? and role = 1", [result.id]);
-      return {... result, dealer:dealers.map(dealer => dealer.name), supporter: supporters.map(supporter => supporter.name)}
-    }))
+    const [results, field] = await connection.query(
+      "SELECT id, contents, party_name, start_time, cnt FROM party_member_count where start_time BETWEEN ? and ? ORDER BY start_time",
+      [dateFormat(now), dateFormat(new Date(now.setDate(now.getDate() + 7)))]
+    );
+    const result = await Promise.all(
+      results.map(async (result) => {
+        const [dealers, dealerFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 0",
+          [result.id]
+        );
+        const [supporters, supporterFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 1",
+          [result.id]
+        );
+        return {
+          ...result,
+          dealer: dealers.map((dealer) => dealer.name),
+          supporter: supporters.map((supporter) => supporter.name),
+        };
+      })
+    );
     return result;
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 
 const findAllPartyByContent = async (contentId) => {
   try {
     let tempResult;
     const now = new Date();
-    const [results, field] = await connection.query("SELECT id, contents, party_name, start_time, cnt FROM party_member_count where contents_id = ? and  start_time BETWEEN ? and ? ORDER BY start_time", [contentId ,dateFormat(now), dateFormat(new Date(now.setDate(now.getDate() + 7)))]);
-    const result = await Promise.all(results.map(async (result) => {
-      const [dealers, dealerFields] = await connection.query("SELECT * from member WHERE party_id = ? and role = 0", [result.id]);
-      const [supporters, supporterFields] = await connection.query("SELECT * from member WHERE party_id = ? and role = 1", [result.id]);
-      return {... result, dealer:dealers.map(dealer => dealer.name), supporter: supporters.map(supporter => supporter.name)}
-    }))
+    const [results, field] = await connection.query(
+      "SELECT id, contents, party_name, start_time, cnt FROM party_member_count where contents_id = ? and  start_time BETWEEN ? and ? ORDER BY start_time",
+      [
+        contentId,
+        dateFormat(now),
+        dateFormat(new Date(now.setDate(now.getDate() + 7))),
+      ]
+    );
+    const result = await Promise.all(
+      results.map(async (result) => {
+        const [dealers, dealerFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 0",
+          [result.id]
+        );
+        const [supporters, supporterFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 1",
+          [result.id]
+        );
+        return {
+          ...result,
+          dealer: dealers.map((dealer) => dealer.name),
+          supporter: supporters.map((supporter) => supporter.name),
+        };
+      })
+    );
     return result;
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 
 const findByDay = async (dayNumber) => {
   let now = new Date();
 
-  while(now.getDay() != dayNumber){
+  while (now.getDay() != dayNumber) {
     now = new Date(now.setDate(now.getDate() + 1));
   }
-  try{
-    const [results, field] = await connection.query("SELECT id, contents, party_name, start_time, cnt FROM party_member_count WHERE DATE(start_time) = ? ORDER BY start_time", [dateFormat(now)]);
+  try {
+    const [results, field] = await connection.query(
+      "SELECT id, contents, party_name, start_time, cnt FROM party_member_count WHERE DATE(start_time) = ? ORDER BY start_time",
+      [dateFormat(now)]
+    );
 
-    const result = await Promise.all(results.map(async (result) => {
-      const [dealers, dealerFields] = await connection.query("SELECT * from member WHERE party_id = ? and role = 0", [result.id]);
-      const [supporters, supporterFields] = await connection.query("SELECT * from member WHERE party_id = ? and role = 1", [result.id]);
-      return {... result, dealer:dealers.map(dealer => dealer.name), supporter: supporters.map(supporter => supporter.name)}
-    }))
+    const result = await Promise.all(
+      results.map(async (result) => {
+        const [dealers, dealerFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 0",
+          [result.id]
+        );
+        const [supporters, supporterFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 1",
+          [result.id]
+        );
+        return {
+          ...result,
+          dealer: dealers.map((dealer) => dealer.name),
+          supporter: supporters.map((supporter) => supporter.name),
+        };
+      })
+    );
     console.log(result);
 
     return result;
-  }catch (e) {
+  } catch (e) {
     console.log(e);
   }
-}
+};
 
+const findByName = async (name) => {
+  try {
+    const now = new Date();
+    const [results, field] = await connection.query(
+      "SELECT id, contents, party_name, start_time, cnt FROM party_member_count where party_name like ? and  start_time BETWEEN ? and ? ORDER BY start_time",
+      [
+        "%" + name + "%",
+        dateFormat(now),
+        dateFormat(new Date(now.setDate(now.getDate() + 7))),
+      ]
+    );
+    const result = await Promise.all(
+      results.map(async (result) => {
+        const [dealers, dealerFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 0",
+          [result.id]
+        );
+        const [supporters, supporterFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 1",
+          [result.id]
+        );
+        return {
+          ...result,
+          dealer: dealers.map((dealer) => dealer.name),
+          supporter: supporters.map((supporter) => supporter.name),
+        };
+      })
+    );
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+
+
+const findByIds = async (partyIds) => {
+  try {
+    const now = new Date();
+    const [results, field] = await connection.query(
+      "SELECT id, contents, party_name, start_time, cnt FROM party_member_count where id in ? and  start_time BETWEEN ? and ? ORDER BY start_time",
+      [
+        partyIds,
+        dateFormat(now),
+        dateFormat(new Date(now.setDate(now.getDate() + 7))),
+      ]
+    );
+    const result = await Promise.all(
+      results.map(async (result) => {
+        const [dealers, dealerFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 0",
+          [result.id]
+        );
+        const [supporters, supporterFields] = await connection.query(
+          "SELECT * from member WHERE party_id = ? and role = 1",
+          [result.id]
+        );
+        return {
+          ...result,
+          dealer: dealers.map((dealer) => dealer.name),
+          supporter: supporters.map((supporter) => supporter.name),
+        };
+      })
+    );
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 function dateFormat(source) {
-  return source.getFullYear() + "-" + (source.getMonth() + 1) + "-" + source.getDate() ;
+  return (
+    source.getFullYear() +
+    "-" +
+    (source.getMonth() + 1) +
+    "-" +
+    source.getDate()
+  );
 }
 
-
-const partyService = {addParty, findAllParty, findByDay, findAllPartyByContent};
+const partyService = {
+  addParty,
+  findAllParty,
+  findByDay,
+  findAllPartyByContent,
+  findByName,
+  findByIds
+};
 
 module.exports = partyService;
