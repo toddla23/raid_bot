c4229b5bf7ba8cfbda89a1c0ede7384d7439f296
toddla23 <qkrwhdgur0319@gmail.com>
2025-09-14 02:04:34 +0900

    [Fix] 컨텐츠명 수정

--- src/util/content.js
+++ src/util/content.js
@@ -1,17 +1,28 @@
 const content = [
   {
-    name: "발탄",
-    value: 1
+    name: "서막: 붉어진 백야의 나선",
+    value: 1,
   },
   {
-    name: "비아키스",
-    value: 2
+    name: "1막 : 대지를 부수는 업화의 궤적",
+    value: 2,
   },
   {
-    name: "쿠크세이튼",
-    value: 3
+    name: "2막 : 부유하는 악몽의 진혼곡",
+    value: 3,
   },
-
-]
+  {
+    name: "3막 : 칠흑, 폭풍의 밤",
+    value: 4,
+  },
+  {
+    name: "4막 : 파멸의 성채",
+    value: 5,
+  },
+  {
+    name: "종막 : 최후의 날",
+    value: 6,
+  },
+];
 
 module.exports = content;
